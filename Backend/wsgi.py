ef582e2dfa597abcf35dd3ba2afc9666efe31edb
Jonathan <jonii10lea@gmail.com>
2024-12-06 18:48:58 -0300

    Verificar estructura y configuración

--- Backend/wsgi.py
+++ Backend/wsgi.py
@@ -1,5 +1,6 @@
 import os
 import sys
+from django.core.wsgi import get_wsgi_application
 
 # Añadir el directorio de la aplicación a PYTHONPATH
 sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
