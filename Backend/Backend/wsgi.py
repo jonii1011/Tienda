f242981ffc0f583fa93284e928b3ff9f4ec52833
Jonathan <jonii10lea@gmail.com>
2024-12-06 15:25:57 -0300

    Despliegue

--- Backend/Backend/wsgi.py
+++ Backend/Backend/wsgi.py
@@ -1,7 +1,12 @@
 import os
+import sys
+
+# Añadir el directorio de la aplicación a PYTHONPATH
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Backend.settings')
+
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Backend.Backend.settings')
-
 application = get_wsgi_application()
+
 
